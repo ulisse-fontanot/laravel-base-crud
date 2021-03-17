96bb4654c18475686c8ed346ff04af455ef5608c
ulisse-fontanot <ulissef99@gmail.com>
2021-03-17 16:03:27 +0100

    init-non va home

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Vestiti</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
+</header>
